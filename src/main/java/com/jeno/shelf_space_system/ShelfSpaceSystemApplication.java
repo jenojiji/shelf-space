01fcf0a67dbce0a0370737529c3320c86699b0d2
JENO JIJI <jenojiji001@gmail.com>
2024-10-28 19:08:37 +0530

    Initial project setup

---
+++ src/main/java/com/jeno/shelf_space_system/ShelfSpaceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jeno.shelf_space_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfSpaceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfSpaceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeno/shelf_space_system/ShelfSpaceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeno.shelf_space_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfSpaceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
